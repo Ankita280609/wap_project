3a6c4f67905e6cc255e28b3221a5ae04a70aef7c
Ankita280609 <thakurankita773@gmail.com>
2025-05-02 20:55:39 +0000

    layout

---
+++ src/components/Navbar.js
@@ -0,0 +1,20 @@
+import React from "react"
+import "./Navbar.css"
+const Navbar=()=>{
+    return (
+        <nav>
+            <div className="logo">
+                <a href="#">Logo</a>
+            </div>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="#">Help</a></li>
+                <li><a href="#" className="login">Login</a></li>
+                <li><a href="#" className="signup">Signup</a></li>
+            </ul>
+        </nav>
+    );
+};
+export default Navbar;
