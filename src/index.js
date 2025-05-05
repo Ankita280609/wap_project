50a1dbb43ee92de31c905b80c43f858507bbc83d
Ankita Thakur <ankitathakur@Ankitas-MacBook-Pro.local>
2025-05-05 19:34:51 +0530

    index

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+import "./index.css"
+    ReactDOM.render(
+        <React.StrictMode>
+            <App/>
+        </React.StrictMode>,
+        document.getElementById("root")
+    )
